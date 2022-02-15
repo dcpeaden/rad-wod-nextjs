a1872d7b144220727630e76b5132e4592638beb4
Cole Peaden <dcpeaden@gmail.com>
2022-02-14 21:54:49 -0600

    add import for 2 col template in wod index

--- pages/wod/index.tsx
+++ pages/wod/index.tsx
@@ -1,11 +1,12 @@
 import { ChakraProvider, Text } from "@chakra-ui/react"
 import Navbar from "../../components/templates/navbar";
+import TwoColWithImageComponent from "../../components/templates/two-columns-with-image";
 
 const WodLandingPage = () => {
   return (
     <ChakraProvider>
       <Navbar />
-      <Text>I actually work!</Text>
+      <TwoColWithImageComponent />
     </ChakraProvider>
   );
 };
