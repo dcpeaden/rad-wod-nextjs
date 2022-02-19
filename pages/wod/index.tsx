4ac155cf6c82a3b376911534ebf5001095f1c991
Cole Peaden <dcpeaden@gmail.com>
2022-02-18 23:09:23 -0600

    P1/setup route to wod (#3)
    * change links and colors
    
    * add wod landing page
    
    * package changes
    
    * introduce new templates and modify 2 col template
    
    * add import for 2 col template in wod index
    
    * copyright free image addition
    
    * remove unnecessary ChakraProvider and unused imports
    
    * ignore vscode files

---
+++ pages/wod/index.tsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import Navbar from "../../components/templates/navbar";
+import TwoColWithImageComponent from "../../components/templates/two-columns-with-image";
+
+const WodLandingPage = () => {
+  return (
+    <Component>
+      <Navbar />
+      <TwoColWithImageComponent />
+    </Component>
+  );
+};
+
+export default WodLandingPage;
